6525aa408253322d2e2498d0809edf81da275615
hagrunes <hagrunes@gmail.com>
2022-04-05 23:50:44 +0900

    init

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,16 +7,22 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//
+////        MemberService memberService = new MemberServiceImpl(memberRepository);
+////        OrderService orderService = new OrderServiceImpl(memberRepository, discountPolicy);
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
-//        MemberService memberService = new MemberServiceImpl(memberRepository);
-//        OrderService orderService = new OrderServiceImpl(memberRepository, discountPolicy);
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
--- src/main/java/hello/core/discount/DiscountPolicy.java
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -8,7 +8,7 @@ public interface DiscountPolicy {
      *
      * @return 할인 대상 금액
      */
-    int discount(Member member, int pirce);
+    int discount(Member member, int price);
 
 
 }
